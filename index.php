07af5e3ebacb4774e37972c921f976991ed36040
Busna27 <lucabusnelli2001@gmail.com>
2022-04-19 16:21:18 +0200

    added strings lenght

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
     $string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam mollitia quos ut molestiae sapiente facilis a unde, laborum iusto quidem, similique corporis. Laudantium ipsum officia saepe nihil eligendi a mollitia!';
-
     $stringBanned = str_replace($_GET["bannedWord"], "***", $string);
+
+    $lenghtBase = strlen($string);
+    $lenghtBanned = strlen($stringBanned);
 ?>
 
 <!DOCTYPE html>
@@ -14,6 +16,8 @@
 </head>
 <body>
     <h3><?php echo $string ?></h3>
+    <h1>La lunghezza: <?php echo $lenghtBase ?></h1>
     <h3><?php echo $stringBanned ?></h3>
+    <h1>La lunghezza: <?php echo $lenghtBanned ?></h1>
 </body>
 </html>
